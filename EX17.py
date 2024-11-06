2dd3d6d7ef2cfb063bceff271bfb6136aff84d67
Morena Zilberman Tavares <morenazilberman@gmail.com>
2024-11-06 16:58:22 -0300

    Update EX17.py

--- EX17.py
+++ EX17.py
@@ -1,4 +1,4 @@
-p = int(input('Digite o valor do peso: '))
+p = int(input('Digite o valor do p: '))
 s = S(input('Determine o sexo da pessoa (masculino/feminino)'))
 a = int(input('Digite o valor da altura:'))
 
@@ -6,20 +6,20 @@ R = p/(a*a)
 
 #feminino
 if (R<19) and (s=feminino):
-    print(f'a pessoa está abaixo do peso  {R}')
+    print(f'a pessoa está abaixo do p  {R}')
 
 elif (R>19) and (R< 24):
     print(f' a pessoa está no peso ideal {R}')
 
 else:
-    print(f'a pessoa está acima do peso {R}')
+    print(f'a pessoa está acima do p {R}')
     
 #masculino
 if  ((R <20) and (s=masculino)):
-    print(f'a pessoa está abaixo do peso  {R}')
+    print(f'a pessoa está abaixo do p  {R}')
 
 elif ((R>20) and (R< 25)):
     print(f' a pessoa está no peso ideal {R}')
 
 else:
-    print(f'a pessoa está acima do peso {R}')
+    print(f'a pessoa está acima do p {R}')
